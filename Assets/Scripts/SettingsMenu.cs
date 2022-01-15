5060e515629567b77e98d2257c0ec55e6b0b0bd7
Bryant Zhang <zhnxbr@gmail.com>
2022-01-15 02:34:42 -0800

    added pause menu with options menu

--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -2,13 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 public class SettingsMenu : MonoBehaviour
 {
+    public static bool PauseGame = false;
+    public GameObject pauseUI;
     // Start is called before the first frame update
-    
-    void Start()
+
+    void Update()
     {
-        QualitySettings.SetQualityLevel(0);
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(PauseGame)
+            {
+                Debug.Log("Resuming!");
+                Resume();
+            }
+            else
+            {
+                Debug.Log("Paused!");
+                Pause();
+            }
+        }
+    }
+    public void Pause()
+    {
+        pauseUI.SetActive(true);
+        Time.timeScale = 0f;
+        PauseGame = true;
+    }
+
+    public void Resume()
+    {
+        pauseUI.SetActive(false);
+        Time.timeScale = 1f;
+        PauseGame = false;
+    }
+    public void Quit()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
 
 }
